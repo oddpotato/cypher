c0e375b7c1baba553ff185e95fb689b9cdce2068
Fiona Apps <wikipanyd@gmail.com>
2021-12-05 11:41:19 +0000

    setting up project

--- README.md
+++
@@ -1,2 +0,0 @@
-# cypher
-Attempting to build a vuejs app allowing user to make basic cypher
---
+++ cypherproject/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <form>
+    <input type="text" v-bind="userInput">
+    <button>Submit Your Entry</button>
+  </form>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data (){
+    return {
+      userInput: ''
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
