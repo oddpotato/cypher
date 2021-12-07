13814bddcb7528225370b0294175fc4796ae33f9
Fiona Apps <wikipanyd@gmail.com>
2021-12-07 12:06:58 +0000

    added routes

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import VueAuth0Plugin from 'vue-auth0-plugin';
-import { domain, clientId } from '../auth_config.json';
-
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
@@ -14,18 +11,6 @@ import StandardButton from './components/styling/StandardButton.vue'
 import CustomFooter from './components/styling/CustomFooter.vue'
 
 const myApp = createApp(App).use(router)
-
-myApp.use(VueAuth0Plugin, {
-    domain,
-    clientId,
-    onRedirectCallback: (appState) => {
-        router.push(
-            appState && appState.targetUrl ?
-            appState.targetUrl :
-            window.location.pathname,
-        );
-    },
-});
 
 myApp.component('standard-button', StandardButton)
 myApp.component('custom-header', CustomHeader)
