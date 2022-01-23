0e7d43c0bc468c163ec83d420cd07f3b48ff63f9
Fiona Apps <wikipanyd@gmail.com>
2022-01-23 12:41:47 +0000

    Moving logic to store

--- src/components/data/InputForm.vue
+++ src/components/data/InputForm.vue
@@ -5,52 +5,31 @@
     </div>
       <form @submit.prevent="submitForm()">
       <textarea label="OG Text" v-model="enteredData"></textarea>
-      <button @click="encode(enteredData)">Encode</button>
+      <!-- <button @click="encode(enteredData)">Encode</button> -->
+      <button @click="encode2(enteredData)">Encode2</button>
       <p>This button turns your original text into the relevant indices if you assume A = 1</p>
     </form>
     <!-- {{enteredData}} -->
   </div>
   <div class="container">
-    {{ correct_indices }}
+    {{ this.$store.state.correct_indices }}
   </div>
 </template>  
 
 <script>
 
-// import { mapGetters } from 'vuex'
-
 export default {
   data() {
     return { 
     enteredData: '',
-    formattedData: '',
-    final_array: [],
-    sanitized_text_array: [],
-    correct_indices: ''
     };
   },
   methods: {
     submitForm() {
       console.log(this.enteredData)
     },
-    encode(text) {
-    console.log('I promise this is running')
-    console.log(text)
-    const regex = /[^A-Za-z]/g;
-    console.log(this.sanitized_text_array + 'Well this is before');
-    this.sanitized_text_array = text.replace(regex,"").toLowerCase().split("");
-    console.log(this.sanitized_text_array);
-    for (const element of this.sanitized_text_array){
-      console.log("Now it's going through some stuff1");
-    for (const item of this.$store.state.the_alphabet_array){
-      console.log("Now it's going through some stuff2");
-      if(element === item){
-        console.log("It's a match!");
-      this.final_array.push(this.$store.state.the_alphabet_array.indexOf(item));
-      console.log(`${element} is at ${this.$store.state.the_alphabet_array.indexOf(item)}`)}    
-    }}
-    this.correct_indices = this.final_array.map(x => x + 1).join(" ")
-    console.log(this.correct_indices)
+    encode2(text){
+      this.$store.commit('encode', text)
     }
   },
 };
--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,38 @@ import { createStore } from 'vuex'
 
 export default createStore({
     state: {
-        the_alphabet_array: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        the_alphabet_array: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+        formattedData: '',
+        final_array: [],
+        sanitized_text_array: [],
+        correct_indices: '',
+        regex: /[^A-Za-z]/g
     },
-    mutations: {},
-    actions: {},
+    mutations: {
+        encode(state, text) {
+            console.log('I promise this is running')
+            console.log(text)
+            console.log(state.sanitized_text_array + 'Well this is before');
+            state.sanitized_text_array = text.replace(state.regex, "").toLowerCase().split("");
+            console.log(state.sanitized_text_array);
+            for (const element of state.sanitized_text_array) {
+                console.log("Now it's going through some stuff1");
+                for (const item of state.the_alphabet_array) {
+                    console.log("Now it's going through some stuff2");
+                    if (element === item) {
+                        console.log("It's a match!");
+                        state.final_array.push(state.the_alphabet_array.indexOf(item));
+                        console.log(`${element} is at ${state.the_alphabet_array.indexOf(item)}`)
+                    }
+                }
+            }
+            state.correct_indices = state.final_array.map(x => x + 1).join(" ")
+            console.log(state.correct_indices)
+
+        }
+    },
+    actions: {
+
+    },
     modules: {}
 })
